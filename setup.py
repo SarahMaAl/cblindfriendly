911597e0d67475674ce554f37cbbcca8728974b8
Sarah MA <143753714+SarahMaAl@users.noreply.github.com>
2024-11-14 18:45:26 +0100

    build(setup): add initial setup configuration for library packaging
    - Configure `setup.py` with library metadata, including name, version, author, and license.
    - Set dependencies with `install_requires`, specifying `matplotlib` and `numpy`.
    - Add `long_description` from `README.md` with explicit UTF-8 encoding for compatibility.
    - Define Python version requirement as >=3.6 in `python_requires`.
    - Link repository URL and add classifiers for programming language, license, and OS support.
    
    This setup configuration allows the library to be easily installed and distributed, ensuring compatibility and accessibility.

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup, find_packages
 
+# Lire le contenu de README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="cblindfriendly",  # Nom de ta librairie
-    version="0.1.0",       # Version de ta librairie
+    name="cblindfriendly",  # Nom de la librairie
+    version="0.1.0",       # Version de la librairie
     packages=find_packages(),  # Trouve automatiquement tous les packages dans le dossier color_palette
-    install_requires=[       # Liste des dépendances nécessaires (ici, matplotlib)
+    install_requires=[       # Liste des dépendances nécessaires
         "matplotlib",
+        "numpy"
     ],
-    classifiers=[  # Pour décrire ta librairie
+    classifiers=[  # Pour décrire la librairie
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    description="Une librairie de palettes de couleurs contrastées et accessibles",
-    long_description=open('README.md').read(),
+    description="Une librairie de palettes de couleurs contrastées et accessibles pour les utilisateurs daltoniens.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Sarah MARTIN-ALONSO",
     author_email="sarah.martin.alonso@gmail.com",
     url="https://github.com/SarahMaAl/cblindfriendly", 
     license="MIT",
+    python_requires=">=3.6",
 )
